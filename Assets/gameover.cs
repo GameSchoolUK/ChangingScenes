80554df10ecf7a4b02a8df5532dd83527b865c49
Pandelis Zembashis <info@pandelis.me>
2014-11-18 19:56:16 +0000

    first commit

---
+++ Assets/gameover.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameover : MonoBehaviour {
+
+	float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timer += Time.deltaTime;
+		if (timer > 5.0)
+		{
+			Application.LoadLevel("main");
+		}
+	}
+}
---
+++ Assets/levelselect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class levelselect : MonoBehaviour {
+
+	public Rect button = new Rect(15,15, 200,110);
+	public string buttonLabel = "Load Level";
+	public string levelToLoad = "Character Basics";
+
+
+	private void OnGUI(){
+		if (GUI.Button(new Rect(15,15, 200,110), "Load Level 1")){
+			Application.LoadLevel("Character Basics");
+		}
+	}
+}
